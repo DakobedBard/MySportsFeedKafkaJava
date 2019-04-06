c402038e43715ee1c3da9cc87c4ca793b982db26
JasonHeyward <mddarr@gmail.com>
2019-04-06 08:49:07 -0700

    Add initial project files.

---
+++ src/main/java/com/github/mddarr/kafka/producer/sportsfeed/HttpRestClient.java
@@ -0,0 +1,14 @@
+package com.github.mddarr.kafka.producer.sportsfeed;
+
+/*
+
+This class will be responsbible for making requests to the MySportsFeed API...
+
+
+
+ */
+
+
+
+public class HttpRestClient {
+}
